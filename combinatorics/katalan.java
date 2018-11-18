1227052817f1a5f8a5a53bc3d66d5241c2fb5bd0
Timur Dautov <tmdautov@gmail.com>
2018-11-18 11:42:50 +0600

    Update katalan.java

--- combinatorics/katalan.java
+++ combinatorics/katalan.java
@@ -30,6 +30,24 @@ class CatalnNumber {
         return res; 
     } 
   
+    int catalanDP(int n) {
+      // Table to store results of subproblems 
+      int[] catalan =  new int[n+1]; 
+  
+      // Initialize first two values in table 
+      catalan[0] = catalan[1] = 1; 
+
+      // Fill entries in catalan[] using recursive formula 
+      for (int i=2; i<=n; i++) { 
+          catalan[i] = 0; 
+          for (int j=0; j<i; j++) 
+              catalan[i] += catalan[j] * catalan[i-j-1]; 
+      } 
+  
+      // Return last entry 
+      return catalan[n]; 
+    }
+  
     public static void main(String[] args) { 
         CatalnNumber cn = new CatalnNumber(); 
         for (int i = 0; i < 10; i++) { 
